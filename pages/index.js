8fcca9847537318c8fec2e5b9b099e2cd1974e12
codemzy <codemzy@gmail.com>
2018-09-18 14:49:24 +0000

    Added year to title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 // components
 import Layout from '../components/layout';
 
+let currentYear = "2018";
+
 export default () => (
-  <Layout title={"2018"}>
-    <div>Welcome to simple-calendar!</div>
+  <Layout title={currentYear}>
+    <div className="container">
+      <h1 className="display-1 font-weight-bold">{currentYear}</h1>
+      <p>Welcome to simple-calendar!</p>
+    </div>
   </Layout>
 );
