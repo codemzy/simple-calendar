6fe6bc8abc8c81749fc45b73c0eb49243cf312f7
codemzy <codemzy@gmail.com>
2018-09-18 14:35:17 +0000

    Added layout component for head

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default () => <div>Welcome to next.js!</div>
+// components
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout title={"2018"}>
+    <div>Welcome to simple-calendar!</div>
+  </Layout>
+);
