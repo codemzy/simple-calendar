d5541b33fde8cbcf75d2ed8fea2d304fc3dbb374
codemzy <codemzy@gmail.com>
2018-09-19 15:33:48 +0000

    Added day component and wired up to month

---
+++ components/year/day.js
@@ -0,0 +1,7 @@
+
+export default ({ day, month }) => {
+  return (
+    <div className="year-day d-inline-block">
+    </div>
+  );
+};
--- components/year/month.js
+++ components/year/month.js
@@ -1,15 +1,21 @@
 import moment from 'moment';
 
+// components
+import Day from './day';
+
 export default ({ month, year }) => {
   
   let monthName = moment(month, "MM").format("MMM");
   let daysInMonth = moment(year + "-" + month, "YYYY-MM").daysInMonth();
   
+  let days = Array.apply(0, Array(daysInMonth)).map(function(item, i) {
+    return <Day key={i} day={i + 1} month={monthName} />;
+  });
+  
   return (
-    <div className="col-md-3">
+    <div className="col-sm-6 col-md-4 col-lg-3">
       <h2>{ monthName }</h2>
-      <p>{ daysInMonth }</p>
-
+      <div className="days">{ days }</div>
     </div>
   );
 };
