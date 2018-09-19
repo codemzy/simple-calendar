29c2a8356747dd1e501553440d78bce44f6b6ab9
codemzy <codemzy@gmail.com>
2018-09-19 16:54:39 +0000

    Added dummy popover component

---
+++ components/popover.js
@@ -0,0 +1,7 @@
+export default ({}) => (
+  <div className="popover fade show bs-popover-top" role="tooltip" x-placement="top" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(325px, 1787px, 0px);">
+    <div className="arrow" style="left: 124px;"></div>
+    <h3 className="popover-header"></h3>
+    <div className="popover-body">Monday, Month 01, Year</div>
+  </div>
+);
