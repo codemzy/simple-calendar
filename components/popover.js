09113c48d3848bc051ffb66fabeb5ed1b50ace03
codemzy <codemzy@gmail.com>
2018-09-20 12:27:00 +0000

    Added popover and method to show on mouse enter

--- components/popover.js
+++ components/popover.js
@@ -1,7 +1,6 @@
-export default ({}) => (
-  <div className="popover fade show bs-popover-top" role="tooltip" x-placement="top" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(325px, 1787px, 0px);">
-    <div className="arrow" style="left: 124px;"></div>
-    <h3 className="popover-header"></h3>
+export default () => (
+  <div className="popover fade show bs-popover-top" role="tooltip" x-placement="top">
+    <div className="arrow" style={{left: "124px"}}></div>
     <div className="popover-body">Monday, Month 01, Year</div>
   </div>
 );
--- components/year/day.js
+++ components/year/day.js
@@ -1,7 +1,29 @@
+import React from 'react';
 
-export default ({ day, month, blank }) => {
-  return (
-    <div className={"d-inline-block year-day" + (blank ? " blank" : "") }>
-    </div>
-  );
+// components
+import Popover from '../popover';
+  
+class Day extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopover: false
+    };
+  }
+  
+  _showPopover(show) {
+    this.setState({
+      showPopover: show
+    });
+  }
+  
+  render() {
+    return (
+      <div className={"d-inline-block year-day" + (this.props.blank ? " blank" : "") } onMouseEnter={this._showPopover.bind(this, true)} onMouseLeave={this._showPopover.bind(this, false)}>
+        { this.state.showPopover ? <Popover /> : false }
+      </div>
+    );
+  }
 };
+
+export default Day;
