7843e4e665f947f85dbc147805ae4b615e65e26f
codemzy <codemzy@gmail.com>
2018-09-20 14:04:57 +0000

    Wired up popover position

--- components/popover.js
+++ components/popover.js
@@ -1,6 +1,6 @@
-export default ({ children }) => (
-  <div className="popover fade show bs-popover-top" role="tooltip" x-placement="top">
-    <div className="arrow" style={{left: "75px"}}></div>
+export default ({ children, coords }) => (
+  <div className="popover fade show bs-popover-top" role="tooltip" x-placement="top" style={{top: "0px", left: "0px", transform: `translate3d(${coords.left}px, ${coords.top}px, 0px)`}}>
+    <div className="arrow" style={{left: "22px"}}></div>
     <div className="popover-body">{ children }</div>
   </div>
 );
--- components/year/day.js
+++ components/year/day.js
@@ -7,20 +7,26 @@ class Day extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showPopover: false
+      showPopover: false,
+      coords: {}
     };
   }
   
-  _showPopover(show) {
+  _showPopover(show, e) {
+    const coords = {
+      top: e.target.offsetTop - (e.target.offsetHeight * 2),
+      left: e.target.offsetLeft - e.target.offsetWidth
+    }
     this.setState({
-      showPopover: show
+      showPopover: show,
+      coords: show ? coords : {}
     });
   }
   
   render() {
     return (
       <div className={"d-inline-block year-day" + (this.props.blank ? " blank" : "") } onMouseEnter={this._showPopover.bind(this, true)} onMouseLeave={this._showPopover.bind(this, false)}>
-        { this.state.showPopover ? <Popover>{this.props.dayName}, {this.props.month} {this.props.day}, {this.props.year}</Popover> : false }
+        { this.state.showPopover ? <Popover coords={this.state.coords}>{this.props.dayName} {this.props.month} {this.props.day}, {this.props.year}</Popover> : false }
       </div>
     );
   }
