67b3893ed2f6c48cb8a3196cb9bd4cf44cea1f1c
codemzy <codemzy@gmail.com>
2018-09-27 16:22:15 +0000

    Added favicon

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,8 @@ export default ({ children, title = 'Simple Calendar' }) => (
   <div className="container pt-3">
     <Head>
       <title>{ title }</title>
+      <link rel="shortcut icon" href="./static/favicon.ico" type="image/x-icon"/>
+      <link rel="icon" href="./static/favicon.ico" type="image/x-icon"/>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
