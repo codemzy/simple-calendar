294b868754fd611785a5b8a99ace2562f67b370a
codemzy <codemzy@gmail.com>
2018-09-28 09:13:39 +0000

    Removed font awesome script as not used

--- out/_next/static/commons/manifest.js
+++ out/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "b2e5ab3b364be5a1ff34"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "bd4f2321b204cd6e5c96"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
